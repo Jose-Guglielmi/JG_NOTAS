2f3d1f6c5485a5a941670efa79b2895b90037192
Jose Guglielmi <lufortgamernw@gmail.com>
2025-03-01 21:42:05 -0300

    1

---
+++ src/Components/menu.jsx
@@ -0,0 +1,10 @@
+import { Bolt } from "lucide-react";
+
+export const Menu = () => {
+  return (
+    <nav className="flex justify-between m-4 ">
+      <h1 className=" text-xl text-white">JG-Notas</h1>
+      <Bolt color="#ffff" />
+    </nav>
+  );
+};
---
+++ src/Screens/Inicio.jsx
@@ -0,0 +1,8 @@
+import { Menu } from "./../Components/menu";
+export const Inicio = () => {
+  return (
+    <>
+      <Menu />
+    </>
+  );
+};
